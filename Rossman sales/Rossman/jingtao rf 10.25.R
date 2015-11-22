a6406f9895787d7a01ff4aef60421bc7103bec3f
peisikan <peisikan>
2015-11-22 18:02:46 -0500

    benchmark

--- Rossman sales/Rossman/jingtao rf 10.25.R
+++ Rossman sales/Rossman/jingtao rf 10.25.R
@@ -1,38 +1,90 @@
 
 library(randomForest)
 library(magrittr)
+library(h2o)
+library(ggplot2)
 
-df = read.csv('train.csv')
+h2o.shutdown()
+localH2O = h2o.init(nthreads=-1,max_mem_size='6G')
 
+df = read.csv('train.csv') %>% merge(read.csv('store.csv'), by = 'Store')
+test = read.csv('test.csv') %>% merge(read.csv('store.csv'), by = 'Store')
+
+summary(df)
+summary(test)
+
+df$Store%<>% as.factor
 df$Date%<>%as.Date
+df$DayOfWeek %<>% as.factor
+df$Open%<>% as.factor
+df$Promo%<>% as.factor
+df$SchoolHoliday %<>% as.factor
+df$Promo2 %<>% as.factor
+df$Month = as.character(df$Date, '%m') %>% as.factor
+df$Quarter = switch(df$Month, )
+df[is.na(df$CompetitionDistance), 'CompetitionDistance'] = mean(df$CompetitionDistance, na.rm = T)
 
-dat = df$Date %>% unique
+test$Store%<>% as.factor
+test$Date%<>%as.Date
+test$Open%<>% as.factor
+test$Promo%<>% as.factor
+test$SchoolHoliday %<>% as.factor
+test$Promo2 %<>% as.factor
+# test$month = as.character(test$Date, '%m')
+test[is.na(test$CompetitionDistance), 'CompetitionDistance'] = mean(test$CompetitionDistance, na.rm = T)
 
-idx = df$Date%in% dat[1:(0.7*length(dat))]
 
-df = df[order(df$Date),]
+summary(df)
+summary(test) 
 
-names(df)
+plot(df[df$Store ==2, 'Customers'])
 
-cols = c('Store', 'DayOfWeek', 'Sales', 'Customers', 'Open', 'Promo', 'StateHoliday', "SchoolHoliday")
+#========= Store 1 ===========#
 
-train = df[idx & df$Store %in% c(1:5), cols]
+plot(df[df$Store == 1 & df$Sales !=0, c('Date','Sales')])
 
-test = df[!idx& df$Store %in% c(1:5), cols]
+df$Date %>% summary
+test$Date %>% summary
 
-dim(train)
-dim(test)
+testNum = test$Date %>% unique %>% length
+testStartDate = max(df$Date) - testNum + 1
 
-rf = randomForest(Sales~., train)
+train = df[df$Date < testStartDate, ]
+test = df[df$Date >= testStartDate, ]
 
-rf
+t = df[df$Store == 1 & df$Month == '08',]
+ggplot(t, aes(x = factor(DayOfWeek), y = Sales)) + geom_boxplot()
+
+
+
+#=========== Model ==========#
+
+test$Customers = aggregate(Customers~Store + DayOfWeek + Promo, df, median) %>% 
+  merge(test, by = names(.)[names(.)!= 'Customers'])
+
+train = as.h2o(df)
+
+features<-colnames(train)[!(colnames(train) %in% c('Id',"Date","Sales"))]
+
+rf = h2o.randomForest(features, 'Sales', train)
+
 summary(rf)
-varImpPlot(rf)
 
-pred = predict(rf, test)
+summary(df)
 
-pred%>% head
+testHex<-as.h2o(test)
 
-plot(pred - test$Sales)
+## Get predictions out; predicts in H2O, as.data.frame gets them into R
+predictions<-as.data.frame(h2o.predict(rf,testHex))
 
-rf$oob.times%>%head
+pred <- predictions[,1]
+summary(pred)
+submission <- data.frame(Id=test$Id, Sales=pred)
+
+
+
+pred = aggregate(Sales~Store, df, mean) %>% merge(test, by = 'Store', all.y = T)
+submission <- data.frame(Id=test$Id, Sales=pred$Sales)
+
+write.csv(submission, "h2o_rf.csv",row.names=F)
+
